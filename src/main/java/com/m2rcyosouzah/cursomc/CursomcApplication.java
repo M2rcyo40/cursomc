65cfc2ba2f93d6d968a5cf75f98c6a48a62cff89
m2rcyosouzah <m2rcyo@gmail.com>
2017-10-01 18:46:20 -0400

    Projeto Criado

---
+++ src/main/java/com/m2rcyosouzah/cursomc/CursomcApplication.java
@@ -0,0 +1,12 @@
+package com.m2rcyosouzah.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+}
